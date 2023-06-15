488f55211b4d815ba801ccb88cb5e733f35e839e
fmancilla <fedemancilla00@gmail.com>
2023-06-15 20:18:16 -0300

    Select empresa mejorado

--- src/components/ListaProveedores.jsx
+++ src/components/ListaProveedores.jsx
@@ -11,8 +11,8 @@ export default function ListaProveedores({handle, register, selected}) {
   
   return (
 
-    <select autoFocus required {...register('head.empresa')} className={ (selected ? '' : ' border-2 border-b-red-500 outline-2 outline-red-500') + ' p-1 m-1 bg-white text-center px-3 w-64 rounded-sm text-base'} onChange={handle} name="" id="">
-      <option disabled selected value="none">Seleccione una opcion</option>
+    <select autoFocus required {...register('head.empresa')} defaultValue='seleccion' className={(selected ? '' : ' border-2 border-b-red-500 outline-2 outline-red-500') + ' p-1 m-1 bg-white text-center px-3 w-64 rounded-sm text-base'} onChange={handle} name="" id="">
+      <option disabled value="seleccion">Seleccione uno</option>
       {proveedores.map(proveedor => {
         return <option className='p-1 m-1' value={proveedor.razon} key={proveedor.razon}>{proveedor.razon}</option>
       })}
